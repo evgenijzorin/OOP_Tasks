21296053899b8641353a4ba3ddbc2b6bec2943c1
“[Зорин <[evgenijzorin@bk.ru]>
2021-11-16 14:10:50 +0300

    выполнены задания 3, 4, 5 из практической работы второго урока

--- Lesson_2/Lesson_2/BankAccount.cs
+++ Lesson_2/Lesson_2/BankAccount.cs
@@ -15,23 +15,74 @@ namespace Lesson_2
 
     public class BankAccount
     {
-        private static long _accountNumber = 6000_0000_0000_0000;
+        private static long _accountCountNumber = 6000_0000_0000_0000;
+        private long _accountNumber = 6000_0000_0000_0000;
         private double _accountBalance;
         public AccountType _accountType;
 
-        // public int AccountNumber { get { return _accountNumber; } set { _accountNumber = value; } }
+        public long AccountNumber { get { return _accountNumber; }  }
         public double AccountBalance { get { return _accountBalance; } set { _accountBalance = value; } }
-
         public AccountType AccountType { get { return _accountType; } set { _accountType = value; } }
 
-        public BankAccount()
+        /// <summary>
+        /// Конструктор по умолчанию
+        /// </summary>
+        public BankAccount() : this(0 , AccountType.debit)
+        {            
+        }
+
+        /// <summary>
+        /// Конструктор с заполнением поля баланс
+        /// </summary>
+        /// <param name="accountBalance"></param>
+        public BankAccount(double accountBalance) : this(accountBalance, AccountType.debit)
         {
-            _accountNumber++ ;
+        }
+
+        /// <summary>
+        /// Основной конструктор
+        /// </summary>
+        /// <param name="accountBalance"></param> 
+        /// <param name="accountType"></param>
+        public BankAccount(double accountBalance, AccountType accountType)
+        {
+            _accountCountNumber++;
+            _accountNumber = _accountCountNumber;
+            _accountBalance = accountBalance;
+            _accountType = accountType;
         }
 
 
+        /// <summary>
+        /// Пополнить счет
+        /// </summary>
+        /// <param name="TopUpSumm"></param>
+        /// <returns></returns>
+        public bool TopUpAccount (double TopUpSumm)
+        {
+            this._accountBalance = _accountBalance + TopUpSumm;
+            return true;
+        }
 
+        /// <summary>
+        /// Снять деньги со счета
+        /// </summary>
+        /// <param name="WithdrawSumm"></param>
+        /// <returns></returns>
+        public bool  WithdrawMoney (double WithdrawSumm)
+        {
+            if (this.AccountBalance >= WithdrawSumm)
+            {
+                this._accountBalance = _accountBalance - WithdrawSumm;
+                return true;
+            }
+            else
+                return false; 
+        }
 
-
+        public void PrintData()
+        {
+            Console.WriteLine($"Номер счета: {AccountNumber} \t Тип счета: {AccountType} \t Баланс счета: {AccountBalance} у.е.");
+        }
     }
 }
--- Lesson_2/Lesson_2/Program.cs
+++ Lesson_2/Lesson_2/Program.cs
@@ -6,13 +6,42 @@ namespace Lesson_2
     {
         static void Main(string[] args)
         {
-            BankAccount bankAccount = new BankAccount();
-            bankAccount.AccountBalance = 300_012.213;           
-            bankAccount.AccountType = AccountType.accumulative;
+            // первый способ 
+            BankAccount bankAccount1 = new BankAccount();
+            bankAccount1.AccountBalance = 300_012.213;           
+            bankAccount1.AccountType = AccountType.accumulative;
+            // 2-й способ 
+            BankAccount bankAccount2 = new BankAccount(200_000);
+            // 3-й способ 
+            BankAccount bankAccount3 = new BankAccount(1_000, AccountType.debit);
 
+            BankAccount[] bankAccounts = new BankAccount[]
+            {
+                bankAccount1,
+                bankAccount2,
+                bankAccount3
+            };
 
+            // Вывод информации:
+            foreach (var item in bankAccounts)
+            {
+                item.PrintData();
+            }
 
+            Console.WriteLine();
+            Console.WriteLine(" Снять деньги 400 у.е. со счета 2 и пополнить счет 3 на 400 у.е.");
 
+            bankAccount2.WithdrawMoney(400);
+            bankAccount3.TopUpAccount(400);
+
+            Console.WriteLine();
+            Console.WriteLine(" Полученный результат:");
+
+            // Вывод информации:
+            foreach (var item in bankAccounts)
+            {
+                item.PrintData();
+            }
         }
     }
 }
