6850867a3719ad6c1c0b4143bef73576506bc4d5
“[Зорин <[evgenijzorin@bk.ru]>
2021-11-16 12:07:26 +0300

    выполнены задания 1, 2 из практической работы второго урока

---
+++ Lesson_2/Lesson_2/BankAccount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_2
+{
+    public enum AccountType
+    {
+        credit,
+        debit,
+        accumulative
+    }
+
+    public class BankAccount
+    {
+        private static long _accountNumber = 6000_0000_0000_0000;
+        private double _accountBalance;
+        public AccountType _accountType;
+
+        // public int AccountNumber { get { return _accountNumber; } set { _accountNumber = value; } }
+        public double AccountBalance { get { return _accountBalance; } set { _accountBalance = value; } }
+
+        public AccountType AccountType { get { return _accountType; } set { _accountType = value; } }
+
+        public BankAccount()
+        {
+            _accountNumber++ ;
+        }
+
+
+
+
+
+    }
+}
---
+++ Lesson_2/Lesson_2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount();
+            bankAccount.AccountBalance = 300_012.213;           
+            bankAccount.AccountType = AccountType.accumulative;
+
+
+
+
+        }
+    }
+}
