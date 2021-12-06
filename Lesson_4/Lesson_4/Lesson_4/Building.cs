11c364ea48605364f4414871c59f33c0b02a9f24
“[Зорин <[evgenijzorin@bk.ru]>
2021-12-06 19:40:02 +0300

    create lesson 4 task

---
+++ Lesson_4/Lesson_4/Lesson_4/Building.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_4
+   {
+   class Building
+      {
+      private int numFloor;
+      private int numFlats;
+      private string street;
+      private int numOfHouse;
+      private int numOfEntrance;
+      private float higthOfBuilding;
+
+      public int NumFloor
+         {
+         get { return numFloor; }
+         set { numFloor = value; }           
+         }
+      public int NumFlats
+         {
+         get { return numFlats; }
+         set { numFlats = value; }
+         }
+      public string Street
+         {
+         get { return street; }
+         set { street = value; }
+         }
+      public int NumOfHouse
+         {
+         get { return numOfHouse; }
+         set { numOfHouse = value; }
+         }
+      public int NumOfEntrance
+         {
+         get { return numOfEntrance; }
+         set { numOfEntrance = value; }
+         }
+      public float HigthOfBuilding
+         {
+         get { return higthOfBuilding; }
+         set { higthOfBuilding = value; }
+         }
+
+      public Building()
+         {
+         }
+      }
+   }
---
+++ Lesson_4/Lesson_4/Lesson_4/flat.cs
@@ -0,0 +1,6 @@
+﻿namespace Lesson_4
+   {
+   internal class flat
+      {
+      }
+   }
