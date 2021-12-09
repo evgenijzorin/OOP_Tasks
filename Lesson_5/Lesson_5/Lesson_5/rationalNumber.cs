07e391ddfc28aa75fb39bce73166700825db884d
“[Зорин <[evgenijzorin@bk.ru]>
2021-12-09 19:41:02 +0300

    create task for lesson 5

---
+++ Lesson_5/Lesson_5/Lesson_5/rationalNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_5
+   {
+   class rationalNumber
+      {
+      private int numerator; // числитель
+      private float denominator; // знаменатель
+
+      public int Numerator
+         {
+         get { return numerator; }
+         set { numerator = value; }
+         }
+      public float Denominator
+         {
+         get { return denominator; }
+         set { denominator = value; }
+         }
+
+
+      }
+   }
