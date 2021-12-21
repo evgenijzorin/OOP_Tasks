a13979cd31a8db7c6a66911d2a568b3d5714452c
“[Зорин <[evgenijzorin@bk.ru]>
2021-12-21 10:36:01 +0300

    Acoder class created.

---
+++ Lesson_7/Interview.Core/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interview.Core
+   {
+   public abstract class Logger
+      {
+      public abstract void ShowMessage(string message);
+      }
+   public sealed class ConsoleLogger : Logger
+      {
+      public override void ShowMessage(string message)
+         {
+         System.Console.WriteLine(message);
+         }
+      }
+   }
--- Lesson_7/Lesson_7/Program.cs
+++ Lesson_7/Lesson_7/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Interview.Core;
 
 namespace Lesson_7
    {
@@ -6,7 +7,15 @@ namespace Lesson_7
       {
       static void Main(string[] args)
          {
-         
+         Logger logger = new ConsoleLogger();
+         ACoder aCoder = new ACoder();
+            {
+            logger.ShowMessage($"Демонстрация работы ACoder: ");
+            string str1 = "абвгдеё";
+            string str2 = aCoder.Encode(str1);
+            string str3 = aCoder.Decode(str2);
+            logger.ShowMessage($"Исходная строка: \"{str1}\"\nЗакодированная строка: \"{str2}\"\nДекодированная строка: \"{str3}");
+            }
          }
       }
    }
