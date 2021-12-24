9916d38c8a60610ce7fc3b1ff14de558a5c75c12
“[Зорин <[evgenijzorin@bk.ru]>
2021-12-24 13:12:33 +0300

    Class BCoder was created; test program

---
+++ Lesson_7/Lesson_7/BCoder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_7
+   {
+   public class BCoder : ICoder
+      {      
+      char[] alphabetArray = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+      // Закодировать
+      public string Encode(string lineEncode)
+         {
+         char[] ar = lineEncode.ToCharArray();
+         for (int i = 0; i < ar.Length; i++)
+            {
+            for (int j = 0; j < alphabetArray.Length; j++)
+               {
+               if (alphabetArray[j] == ar[i])
+               // Замена символа на аналогичный по нумерации с конца
+                  { 
+                  ar[i] = alphabetArray[alphabetArray.Length - j -1];    
+                  break;
+                  }
+               }
+            }
+         return new string(ar);
+         }
+
+      public string Decode(string lineDecode)
+         {
+         char[] ar = lineDecode.ToCharArray();
+         for (int i = 0; i < ar.Length; i++)
+            {
+            for (int j = 0; j < alphabetArray.Length; j++)
+               {
+               if (alphabetArray[j] == ar[i])
+               // Замена символа на аналогичный по нумерации с конца
+                  {
+                  ar[i] = alphabetArray[alphabetArray.Length - j - 1];
+                  break;
+                  }
+               }
+            }
+         return new string(ar);
+         }
+      }
+   }
--- Lesson_7/Lesson_7/Program.cs
+++ Lesson_7/Lesson_7/Program.cs
@@ -16,6 +16,15 @@ namespace Lesson_7
             string str3 = aCoder.Decode(str2);
             logger.ShowMessage($"Исходная строка: \"{str1}\"\nЗакодированная строка: \"{str2}\"\nДекодированная строка: \"{str3}");
             }
+
+         BCoder bCoder = new BCoder();
+            {
+            logger.ShowMessage($"\nДемонстрация работы BCoder: ");
+            string str1 = "абвгдеё";
+            string str2 = bCoder.Encode(str1);
+            string str3 = bCoder.Decode(str2);
+            logger.ShowMessage($"Исходная строка: \"{str1}\"\nЗакодированная строка: \"{str2}\"\nДекодированная строка: \"{str3}");
+            }
          }
       }
    }
